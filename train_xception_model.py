00a1e80041c9d6104cd36cb50a8556ca6c04c4c8
Gant Laborde <gantman@gmail.com>
2019-01-22 08:53:08 -0600

    getting 87 percent with xception

--- train_xception_model.py
+++ train_xception_model.py
@@ -5,24 +5,31 @@ from pathlib import Path
 import numpy as np
 import joblib
 from keras.preprocessing import image
-from keras.applications import VGG16
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.applications import xception
+from keras.models import Sequential, Model, load_model
+from keras.layers import Dense, Dropout, Flatten, GlobalAveragePooling2D
 from keras.callbacks import ModelCheckpoint
 from keras import initializers, regularizers
 
-conv_base = VGG16(
-    weights='imagenet',
-    include_top=False,
-    input_shape=(256, 256, 3)
+# Config
+height = 256
+width = 256
+num_channels = 3
+num_classes = 5
+weights_file = "weights.best_xception.hdf5"
+
+conv_base = xception.Xception(
+      weights='imagenet', 
+      include_top=False, 
+      input_shape=(height, width, num_channels)
 )
 
-# Let's unlock som trainable layers
+# Let's unlock som trainable layers in conv_base
 conv_base.trainable = True
 
 set_trainable = False
 for layer in conv_base.layers:
-    if layer.name == 'block5_conv1':
+    if layer.name == 'block14_sepconv1':
         set_trainable = True
     if set_trainable:
         layer.trainable = True
@@ -33,21 +40,26 @@ for layer in conv_base.layers:
 print('Summary')
 print(conv_base.summary())
 
-model = Sequential([
-    conv_base,
-    Flatten(),
-    Dense(128, activation='relu', kernel_regularizer=regularizers.l2(
-        0.01), kernel_initializer=initializers.he_normal(seed=None)),
-    Dropout(0.5),
-    Dense(5, activation='softmax')
-])
+# Let's construct that top layer replacement
+x = conv_base.layers[-1].output
+x = GlobalAveragePooling2D()(x)
+x = Dense(256, activation='relu', kernel_regularizer=regularizers.l2(
+        0.01), kernel_initializer=initializers.he_normal(seed=None))(x)
+x = Dropout(0.5)(x)
+x = Dense(128,activation='relu', kernel_initializer=initializers.he_normal(seed=None))(x)
+x = Dropout(0.25)(x)
+top_layer = Dense(num_classes, activation='softmax')(x)
 
-if os.path.exists("weights.best.hdf5"):
-        print ("loading ", "weights.best.hdf5")
-        model.load_weights('weights.best.hdf5')
+print('Stacking New Layers')
+model=Model(conv_base.layers[0].input, top_layer)
+
+# Load checkpoint if one is found
+if os.path.exists(weights_file):
+        print ("loading ", weights_file)
+        model.load_weights(weights_file)
 
 # checkpoint
-filepath = "weights.best.hdf5"
+filepath = weights_file
 checkpoint = ModelCheckpoint(
     filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
@@ -75,7 +87,7 @@ validation_datagen = ImageDataGenerator(
     rescale=1./255
 )
 
-GENERATOR_BATCH_SIZE = 32
+GENERATOR_BATCH_SIZE = 8
 base_dir = 'D:\\nswf_model_training_data\\data'
 
 train_dir = os.path.join(base_dir, 'train')
@@ -83,33 +95,36 @@ test_dir = os.path.join(base_dir, 'test')
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
-    target_size=(256, 256),
+    target_size=(height, width),
     class_mode='categorical',
     batch_size=GENERATOR_BATCH_SIZE
 )
 
 validation_generator = validation_datagen.flow_from_directory(
     test_dir,
-    target_size=(256, 256),
+    target_size=(height, width),
     class_mode='categorical',
     batch_size=GENERATOR_BATCH_SIZE
 )
 
-import time
-time.sleep(1)
+# Comment in this line if you're looking to reload the last model for training
+# Essentially, not taking the best validation weights but to add more epochs
+model = load_model('nsfw.h5')
 
 print('Start training!')
 history = model.fit_generator(
     train_generator,
     callbacks=callbacks_list,
-    epochs=50,
-    steps_per_epoch=3000,
+    epochs=100,
+    steps_per_epoch=2000,
     shuffle=True,
     # having crazy threading issues
+    # set workers to zero if you see an error like: 
+    # `freeze_support()`
     workers=0,
     use_multiprocessing=True,
     validation_data=validation_generator,
-    validation_steps=600
+    validation_steps=200
 )
 
 acc = history.history['acc']
