d78dfc5de3f045faffaa618ce88151d79f0de1a8
Gant Laborde <gantman@gmail.com>
2019-02-05 16:00:37 -0600

    use trained model to help clean large dataset

--- self_clense.py
+++ self_clense.py
@@ -1,53 +1,65 @@
+import os
 import numpy as np
 from keras.preprocessing import image
 from pathlib import Path
 from keras.models import load_model
 from sklearn.metrics import confusion_matrix, classification_report
+
+# Initialize
 model = load_model("nsfw.299x299.h5")
-
-# Empty lists to hold the images and labels for each each image
+image_size = 299
+file_count = 0
+accepted_file_count = 0
+mistake_count = 0
 x_test = []
+y_test = []
+categories = ['drawings', 'hentai', 'neutral', 'porn', 'sexy']
 base_dir = 'D:\\nswf_model_training_data\\data'
-group = 'test'
-category = 'drawings'
-file_type = "jpg"
+batch_size = 8000
 
+# CONFIGURE EACH RUN
+group = 'train'
+category_id = 1
+file_type = "jpg"
+page = 1
+
+
+bottom = page * batch_size
+top = (page + 1) * batch_size
 # Load the data set by looping over every image file
 for image_file in Path(base_dir + "\\" + group + "\\" +
-                       category).glob("**/*." + file_type):
+                       categories[category_id]).glob("**/*." + file_type):
+    file_info = {"path": image_file, "filename": os.path.basename(image_file)}
 
-    # Load the current image file
-    image_data = image.load_img(image_file, target_size=(299, 299))
+    file_count += 1
 
-    # Convert the loaded image file to a numpy array
-    image_array = image.img_to_array(image_data)
+    # limit results for in-memmory batches
+    if bottom <= file_count < top:
+        accepted_file_count += 1
+        # Load the current image file
+        image_data = image.load_img(image_file, target_size=(image_size, image_size))
 
-    # Add the current image to our list of test images
-    x_test.append(image_array)
+        # Convert the loaded image file to a numpy array
+        image_array = image.img_to_array(image_data)
+        image_array /= 255
 
-    # Add an expected label for this image. If it was a not_bird image, label it 0. If it was a bird, label it 1.
-    # if "not_bird" in image_file.stem:
-    #     y_test.append(0)
-    # else:
-    #     y_test.append(1)
+        # Add the current image to our list of test images
+        x_test.append(image_array)
+        # To identify failed predictions
+        y_test.append(file_info)
 
 # Convert the list of test images to a numpy array
 x_test = np.array(x_test)
 
-# Make predictions
+# Make predictions (arrays of size 5, with probabilities)
 predictions = model.predict(x_test)
+# print(predictions)
+max_predictions = np.argmax(predictions, axis=1)
 
-print(predictions)
-
-# predictions = predictions > 0.5
-
-# # Calculate how many mis-classifications the model makes
-# tn, fp, fn, tp = confusion_matrix(y_test, predictions).ravel()
-# print(f"True Positives: {tp}")
-# print(f"True Negatives: {tn}")
-# print(f"False Positives: {fp}")
-# print(f"False Negatives: {fn}")
-
-# # Calculate Precision and Recall for each class
-# report = classification_report(y_test, predictions)
-# print(report)
+for idx, prediction in enumerate(max_predictions):
+    if prediction != category_id:
+        mistake_count += 1
+        # print('Thought it was ' + categories[prediction] + ': ' + str(y_test[idx]["path"]))
+        os.rename(y_test[idx]["path"], base_dir + "\\" + group + "\\mistakes\\" + str(y_test[idx]["filename"]))
+        
+print('Out of ' + str(accepted_file_count) + ' images of "' + str(categories[category_id]) + '" ' + str(mistake_count) + ' are failures')
