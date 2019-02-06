c974afd22d7ba20bbff162668c5f4573fe917bb8
Gant Laborde <gantman@gmail.com>
2019-02-05 20:02:41 -0600

    enable batching for mem

--- self_clense.py
+++ self_clense.py
@@ -9,57 +9,68 @@ from sklearn.metrics import confusion_matrix, classification_report
 model = load_model("nsfw.299x299.h5")
 image_size = 299
 file_count = 0
-accepted_file_count = 0
-mistake_count = 0
 x_test = []
 y_test = []
+mistakes = []
 categories = ['drawings', 'hentai', 'neutral', 'porn', 'sexy']
 base_dir = 'D:\\nswf_model_training_data\\data'
-batch_size = 8000
+batch_size = 1000
+page = 0
 
 # CONFIGURE EACH RUN
 group = 'train'
-category_id = 1
+category_id = 2
 file_type = "jpg"
-page = 1
 
+def process_batch(batch_x, batch_y):
+    print("Batch Check " + str(file_count))
+    # Convert the list of images to a numpy array
+    x_array = np.array(batch_x)
 
-bottom = page * batch_size
-top = (page + 1) * batch_size
-# Load the data set by looping over every image file
+    # Make predictions (arrays of size 5, with probabilities)
+    predictions = model.predict(x_array)
+    max_predictions = np.argmax(predictions, axis=1)
+
+    for idx, prediction in enumerate(max_predictions):
+        if prediction != category_id:
+            # log it!
+            mistakes.append(batch_y[idx])
+
+# Copies categorization failures to the mistakes folder for analysis
+def copy_all_failures():
+    for file_info in mistakes:
+        os.rename(file_info["path"], base_dir + "\\" + group + "\\mistakes\\" + str(file_info["filename"]))
+
+print("Starting Self-clense for " + categories[category_id])
+# Load the data set by looping over every image file in path
 for image_file in Path(base_dir + "\\" + group + "\\" +
                        categories[category_id]).glob("**/*." + file_type):
     file_info = {"path": image_file, "filename": os.path.basename(image_file)}
 
+    top = (page + 1) * batch_size
     file_count += 1
 
-    # limit results for in-memmory batches
-    if bottom <= file_count < top:
-        accepted_file_count += 1
-        # Load the current image file
-        image_data = image.load_img(image_file, target_size=(image_size, image_size))
+    # Load the current image file
+    image_data = image.load_img(image_file, target_size=(image_size, image_size))
 
-        # Convert the loaded image file to a numpy array
-        image_array = image.img_to_array(image_data)
-        image_array /= 255
+    # Convert the loaded image file to a numpy array
+    image_array = image.img_to_array(image_data)
+    image_array /= 255
 
-        # Add the current image to our list of test images
-        x_test.append(image_array)
-        # To identify failed predictions
-        y_test.append(file_info)
+    # Add the current image to our list of test images
+    x_test.append(image_array)
+    # To identify failed predictions
+    y_test.append(file_info)
 
-# Convert the list of test images to a numpy array
-x_test = np.array(x_test)
+    # Kick off a processing to clear RAM
+    if file_count == top:
+        process_batch(x_test, y_test)
+        # move next batch moment
+        page += 1
+        # reset in-memory
+        x_test = []
+        y_test = []
 
-# Make predictions (arrays of size 5, with probabilities)
-predictions = model.predict(x_test)
-# print(predictions)
-max_predictions = np.argmax(predictions, axis=1)
-
-for idx, prediction in enumerate(max_predictions):
-    if prediction != category_id:
-        mistake_count += 1
-        # print('Thought it was ' + categories[prediction] + ': ' + str(y_test[idx]["path"]))
-        os.rename(y_test[idx]["path"], base_dir + "\\" + group + "\\mistakes\\" + str(y_test[idx]["filename"]))
-        
-print('Out of ' + str(accepted_file_count) + ' images of "' + str(categories[category_id]) + '" ' + str(mistake_count) + ' are failures')
+process_batch(x_test, y_test)     
+copy_all_failures()   
+print('Out of ' + str(file_count) + ' images of "' + str(categories[category_id]) + '" ' + str(len(mistakes)) + ' are failures')
